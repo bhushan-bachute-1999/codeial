1cfe12f31b2cda23923ad6d3645d48f1e6d9a9de
bhushan.bachute.199 <bhushan.bachute.1999@gmail.com>
2022-12-05 19:15:54 +0530

    First commit: Create first directory structure and setup express and git

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'); // Include module express
+const port = 8000;// Define port
+
+const app = express();
+
+// Listen on port 8000
+app.listen(port, function (err) {
+    if (err) {
+        console.log(`Error :${port}`);
+        return;
+    }
+    console.log(`Server is up and running on port: ${port}`);
+});
