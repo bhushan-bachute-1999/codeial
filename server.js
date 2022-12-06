c43da9deaf4d90018277739a5e90ac6adfbf3a0f
bhushan.bachute.199 <bhushan.bachute.1999@gmail.com>
2022-12-06 14:50:35 +0530

    Sixth commit: Implement layouts

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express'); // Include module express
 const port = 8000;// Define port
+const expressLayouts = require('express-ejs-layouts');// Use layouts library
 
 const app = express();
+
+app.use(expressLayouts);
+app.set('layout', 'layouts');//Select the layout which we want to use
 
 app.set('view engine', 'ejs');//Setup view engine
 app.set('views', './views');//Give path to view folder
